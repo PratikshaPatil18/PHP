9396a2b5763d862f079d6a5f23570ca4d152ea95
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-12 10:18:05 +0530

    if-else-if statement.
    commit here

---
+++ if-else-if statements.py
@@ -0,0 +1,10 @@
+#if-else-if statements.
+marks=88;
+if(marks>=75):
+    print("1st class with dist.");
+elif(marks>=60):
+     print("1st class.");
+elif(marks>=45):
+     print("2n class");
+else:
+    print("oops.. your are fail.");
