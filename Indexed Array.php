d258aa471220a74693b6b80be76077510110933c
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-14 22:55:47 +0530

    Indexed Array
    commit here

---
+++ Indexed Array.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php
+echo"<h2>Indexed or Numeric Array</h2>";
+$a[0]="CO";
+$a[1]="IT";
+$a[2]="ME";
+echo $a[0] ;
+echo"<br>";
+echo $a[1];
+echo"<br>";
+echo $a[2];
+?>
+</body>
+</html>
