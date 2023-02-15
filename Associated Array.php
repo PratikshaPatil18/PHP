79d5868923b56d8761b23765bcb9c8a166973946
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-15 22:20:38 +0530

    Associated Array
    commit here

---
+++ Associated Array.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+echo"<h2>Associated Array</h2>";
+$course=array("CO"=>100,
+			  "IT"=>200,
+			  "ME"=>300);
+echo"course['CO']=",$course["CO"],"<br>";
+echo"course['IT']=",$course["IT"],"<br>";
+echo"course['ME']=",$course["ME"];
+?>
+</body>
+</html>
