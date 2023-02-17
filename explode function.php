51086098726d788953d1a7ee4247f5de9b69647a
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-17 08:08:03 +0530

    Expload Function
    commit here

---
+++ explode function.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+echo"explode function.<br>";
+$str="hello world! have beautiful day";
+$arr=explode(" ",$str,4);
+echo"<pre>";
+print_r($arr);
+echo"</pre>";
+?>
+</body>
+</html>
