bc8e7488ff020a93c16b202247f6ede7a3af21e2
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-03-02 22:56:32 +0530

    string function in php.
    commit here

---
+++ string function.php
@@ -0,0 +1,12 @@
+<?php
+echo '<h2> String Function </h2><br>';
+echo "strlen = welcome to php world :".strlen('welcome to php world.')."<br>";
+echo"str_word_count = welcome to php world :". str_word_count('welcome to php world.')."<br>";
+echo "strrev = welcome to php world :".strrev('welcome to php world.')."<br>";
+echo  "strpos = welcome to php world :".strpos('welcome to php world','php')."<br>";
+echo 'str_replace : '.str_replace('php','coding','welcome php').'<br>';
+echo 'strtoupper :'.strtoupper(' welcome to php world ').'<br>';
+echo 'str_repeat :'.str_repeat('Pratiksha' ,2).'<br>';
+echo 'strcmp (equal) :'.strcmp('Hello php','hello php').'<br>';
+echo 'strcmp (not equal) :'.strcmp('hello php','hello css').'<br>';
+?> 
