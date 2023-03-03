5dde162fea0f5d2f39d9591c0dbef92510ea0540
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-03-03 14:05:29 +0530

    maths function in php
    commit here

---
+++ maths function.php
@@ -0,0 +1,16 @@
+<?php
+echo "abs(-9)".abs(-9);
+echo'<br>';
+
+echo "ceil(5.6)".ceil(5.6);
+echo'<br>';
+
+echo "sqrt(84)".sqrt(8);
+echo'<br>';
+
+echo "floor(5.6)".floor(5.6);
+echo'<br>';
+
+echo "decbin(10)".decbin(10);
+echo'<br>';
+?>
